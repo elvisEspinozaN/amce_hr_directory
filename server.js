56c365f96b640b63233adbd145c4bd5c3e68c980
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-04-10 20:56:15 -0400

    created tables

--- server.js
+++ server.js
@@ -8,6 +8,29 @@ const client = new pg.Client(
 
 const server = express();
 
-async function init() {}
+async function init() {
+  await client.connect();
+  console.log("conencted to db");
+
+  // create tables with foreign key relationships
+  let SQL = `
+    DROP TABLE IF EXISTS employees;
+    DROP TABLE IF EXISTS departments;
+    CREATE TABLE departments(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE employees(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(49) NOT NULL,
+      created_at TIMESTAMP DEFAULT now(),
+      updated_at TIMESTAMP DEFAULT now(),
+      department_id INTEGER REFERENCES departments(id) NOT NULL
+    );
+  `;
+
+  await client.query(SQL);
+  console.log("tables created");
+}
 
 init();
